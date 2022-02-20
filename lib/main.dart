17af147a5cc1c27f522cb3e7bd60742056d58fe8
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-20 20:41:27 +0530

    added theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,65 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const themeSeedColors = [
+  Colors.pink,
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.teal,
+  Colors.blueGrey,
+  Colors.purple
+];
+var currentPlatform = TargetPlatform.android;
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  Color? _seedColor;
+  // This widget is the root of your application.
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _seedColor = themeSeedColors[Random().nextInt(7)];
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My shop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      darkTheme: ThemeData(
+        colorSchemeSeed: _seedColor,
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        platform: currentPlatform,
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle:
+              SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        ),
       ),
+      theme: ThemeData(
+        colorSchemeSeed: _seedColor,
+        brightness: Brightness.light,
+        useMaterial3: true,
+        platform: currentPlatform,
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle:
+              SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
     );
   }
