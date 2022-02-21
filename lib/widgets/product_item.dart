30e642647e9adbd3bc7b09ae77dbc7d67b30c516
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-21 14:27:36 +0530

    13 only wrapped  favourite toggle button with consumer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,70 +13,71 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final product = Provider.of<Product>(context);
-    return Consumer<Product>(
-      builder: (context, product, child) => Card(
-        elevation: 20,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        child: InkWell(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => ProductDetailsScreen(id: product.id),
-            ));
-          },
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: GridTile(
-              child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, imageChunkEvent) {
-                  return imageChunkEvent == null
-                      ? child
-                      : Center(
-                          child: CircularProgressIndicator.adaptive(
-                            strokeWidth: 1,
-                            semanticsLabel: "Loading",
-                            value: (imageChunkEvent.cumulativeBytesLoaded /
-                                imageChunkEvent.expectedTotalBytes!),
-                          ),
-                        );
-                },
-              ),
-              header: GridTileBar(
-                  leading: CircleAvatar(
+    final product = Provider.of<Product>(context, listen: false);
+    return Card(
+      elevation: 20,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ProductDetailsScreen(id: product.id),
+          ));
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: GridTile(
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, imageChunkEvent) {
+                return imageChunkEvent == null
+                    ? child
+                    : Center(
+                        child: CircularProgressIndicator.adaptive(
+                          strokeWidth: 1,
+                          semanticsLabel: "Loading",
+                          value: (imageChunkEvent.cumulativeBytesLoaded /
+                              imageChunkEvent.expectedTotalBytes!),
+                        ),
+                      );
+              },
+            ),
+            header: GridTileBar(
+              leading: CircleAvatar(
                 backgroundColor: Colors.black26,
                 child: BlurryContainer(
                   borderRadius: BorderRadius.circular(20),
                   blur: Consts.kBlur,
                   child: Material(
                     color: Colors.transparent,
-                    child: IconButton(
-                      icon: Icon(
-                        product.isFavourite
-                            ? Icons.favorite_rounded
-                            : Icons.favorite_outline_rounded,
-                        color: Colors.redAccent,
+                    child: Consumer<Product>(
+                      builder: (context, product, child) => IconButton(
+                        icon: Icon(
+                          product.isFavourite
+                              ? Icons.favorite_rounded
+                              : Icons.favorite_outline_rounded,
+                          color: Colors.redAccent,
+                        ),
+                        onPressed: () => product.toggleFavouriteValue(),
                       ),
-                      onPressed: () => product.toggleFavouriteValue(),
                     ),
                   ),
                 ),
-              )),
-              footer: ClipRRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(
-                      sigmaX: Consts.kBlur, sigmaY: Consts.kBlur),
-                  child: GridTileBar(
-                    backgroundColor: Colors.black54,
-                    trailing: IconButton(
-                      icon: const Icon(Icons.shopping_cart_outlined),
-                      onPressed: () {},
-                    ),
-                    title: Text(
-                      product.title,
-                      textAlign: TextAlign.center,
-                    ),
+              ),
+            ),
+            footer: ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                    sigmaX: Consts.kBlur, sigmaY: Consts.kBlur),
+                child: GridTileBar(
+                  backgroundColor: Colors.black54,
+                  trailing: IconButton(
+                    icon: const Icon(Icons.shopping_cart_outlined),
+                    onPressed: () {},
+                  ),
+                  title: Text(
+                    product.title,
+                    textAlign: TextAlign.center,
                   ),
                 ),
               ),
