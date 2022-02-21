e464bb62da32b216af2ec38b82704c5778a77d14
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-21 14:20:34 +0530

    12 migrated to consumer insead of context method

--- lib/models/consts.dart
+++ lib/models/consts.dart
@@ -1,4 +1,7 @@
 class Consts {
   static const double kBlur = 10;
   static const double kRadius = 15;
+  static const bool kReleaseMode = bool.fromEnvironment('dart.vm.product');
+  static const bool kProfileMode = bool.fromEnvironment('dart.vm.profile');
+  static const bool kDebugMode = !kReleaseMode && !kProfileMode;
 }
