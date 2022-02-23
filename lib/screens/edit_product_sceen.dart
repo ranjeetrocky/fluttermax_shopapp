0e9869464867473ca2b2430150f1cfd21c63c1ab
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-23 11:46:25 +0530

    38 added Input Focus on price input

--- lib/screens/edit_product_sceen.dart
+++ lib/screens/edit_product_sceen.dart
@@ -9,6 +9,7 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +18,22 @@ class _EditProductScreenState extends State<EditProductScreen> {
         padding: const EdgeInsets.all(16),
         child: Form(
             child: ListView(
-          children: [TextFormField()],
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(label: Text('Title')),
+              textInputAction: TextInputAction.next,
+              autofocus: true,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_priceFocusNode);
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(label: Text('Price')),
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.number,
+              focusNode: _priceFocusNode,
+            ),
+          ],
         )),
       ),
     );
