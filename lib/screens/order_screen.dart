12f583695669bf9e7b5f3c9e77834bdb76c8faea
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-24 17:47:18 +0530

    55  added optimzation code to avoid unnecessary build calls

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -13,14 +13,22 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  Future<void> _refreshData() async => setState(() {});
+  Future? _ordersFuture;
+  Future _obtainOrdersFuture() =>
+      Provider.of<Orders>(context, listen: false).fetchAndSet();
+  @override
+  void initState() {
+    // TODO: implement initState
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Orders')),
       body: FutureBuilder(
-        future: Provider.of<Orders>(context, listen: false).fetchAndSet(),
+        future: _ordersFuture,
         builder: (context, snapshot) {
           snapshot.hasError
               ? print("Error : " + snapshot.error.toString())
@@ -29,30 +37,27 @@ class _OrderScreenState extends State<OrderScreen> {
           print(snapshot.connectionState);
           return snapshot.connectionState == ConnectionState.waiting
               ? const Center(child: CircularProgressIndicator.adaptive())
-              : RefreshIndicator(
-                  onRefresh: _refreshData,
-                  child: snapshot.hasError
-                      ? Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Text('An error occured'),
-                              ElevatedButton.icon(
-                                  onPressed: () => _refreshData(),
-                                  icon: const Icon(Icons.refresh_rounded),
-                                  label: const Text('Refresh'))
-                            ],
-                          ),
-                        )
-                      : Consumer<Orders>(
-                          builder: (context, orders, child) => ListView.builder(
-                            itemBuilder: (context, index) {
-                              return OrderItem(order: orders.orderItems[index]);
-                            },
-                            itemCount: orders.orderItems.length,
-                          ),
-                        ),
-                );
+              : snapshot.hasError
+                  ? Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Text('An error occured'),
+                          // ElevatedButton.icon(
+                          //     onPressed: () => initState(),
+                          //     icon: const Icon(Icons.refresh_rounded),
+                          //     label: const Text('Refresh'))
+                        ],
+                      ),
+                    )
+                  : Consumer<Orders>(
+                      builder: (context, orders, child) => ListView.builder(
+                        itemBuilder: (context, index) {
+                          return OrderItem(order: orders.orderItems[index]);
+                        },
+                        itemCount: orders.orderItems.length,
+                      ),
+                    );
         },
       ),
     );
